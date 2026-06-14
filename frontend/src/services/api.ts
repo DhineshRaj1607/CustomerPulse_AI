86863380809d1a7f78b066c9c2d7aa95eb4887ce
Dhinesh Raj Marudaiyan <dineshmarudaiyan@gmail.com>
2026-06-14 21:59:50 +0530

    ..

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
-
+console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
 export type Customer = {
   _id: string;
   name: string;
